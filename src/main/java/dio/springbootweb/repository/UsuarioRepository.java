3e48ca7fbf0dd908e5eba7134ff5a87b8eb2f522
stevopablo <stevaopabllo@gmai.com>
2024-10-10 13:12:49 -0300

    final

---
+++ src/main/java/dio/springbootweb/repository/UsuarioRepository.java
@@ -0,0 +1,35 @@
+package dio.springbootweb.repository;
+
+import dio.springbootweb.model.Usuario;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UsuarioRepository {
+    public void save(Usuario usuario){
+        System.out.println("SAVE - Recebendo o usuário");
+        System.out.println(usuario);
+    }
+    public void update(Usuario usuario){
+        System.out.println("UPDATE - Recebendo o usuário ");
+        System.out.println(usuario);
+    }
+    public void remove(Integer id){
+        System.out.println(String.format("DELETE/id - Recebendo o id: %d para excluir um usuário", id));
+        System.out.println(id);
+    }
+    public List<Usuario> listAll(){
+        System.out.println("LIST - Listando os usários ");
+        List<Usuario> usuarios = new ArrayList<>();
+        usuarios.add(new Usuario("emma","password"));
+        usuarios.add(new Usuario("jose","masterpass"));
+        return usuarios;
+    }
+    public Usuario finById(Integer id){
+        System.out.println(String.format("FIND/id - Recebendo o id: %d para localizar um usuário", id));
+        return new Usuario("gleyson","password");
+    }
+
+}
